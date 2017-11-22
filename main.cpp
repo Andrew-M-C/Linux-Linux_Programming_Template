665f9e6ac4d0cbed21756b5f80a6bc0ec72972bd
Andrew-M-C <laplacezhang@126.com>
2017-11-22 21:48:02 +0800

    [DEV] Trying to invoke a func and then return to main func. Now there is an sigmentation fault in my code. There is still many things to be done.

--- main.cpp
+++ main.cpp
@@ -11,9 +11,12 @@ extern "C" {
 #define CFG_LIB_DAEMON
 #define CFG_LIB_SLEEP
 #define CFG_LIB_PID
+#define CFG_LIB_MMAP
+#define CFG_LIB_STRING
 #include "AMCCommonLib.h"
 
 #include <setjmp.h>
+#include <stdint.h>
 
 #if defined (__cplusplus)
 }
@@ -31,44 +34,118 @@ extern "C" {
 #define	CFG_SOFTWARE_VER_STR		"0.0.1"
 #define	CFG_AUTHOR_STR		"Andrew Chang"
 
-
-extern "C" void asm_invoke_func(void *pFunc);
-static void _func_A();
-static void _func_B();
-
-static void _func_test_invoke()
+/*******************************************************
+ * coroutines
+ */
+typedef struct Context
 {
-    AMCPrintf("test asm");
+    uint64_t    rbx;    // 0
+    uint64_t    rsp;    // 8
+    uint64_t    rbp;    // 16
+    uint64_t    r12;    // 24
+    uint64_t    r13;    // 32
+    uint64_t    r14;    // 40
+    uint64_t    r15;    // 48
+    uint64_t    rip;    // 56: return address for coroutine
+    uint64_t    break_point;    // 64
+} Context_st;
+
+
+typedef struct Stack
+{
+    uint8_t     stack[1024];
+} Stack_st;
+
+extern "C" void asm_init_context(Context_st *pCtx);
+extern "C" void asm_save_context(Context_st *pCtx);
+extern "C" void asm_save_main_and_go(Context_st *pMainCtx, Context_st *pCorCtx);
+extern "C" void *asm_get_stack_addr(void);
+
+
+static void _run_coroutines(Context_st *pMainCtx, Context_st *pSubCtx)
+{
+    AMCPrintf("Now go with %p and %p", pMainCtx, pSubCtx);
+    asm_save_main_and_go(pMainCtx, pSubCtx);
+    AMCPrintf("go done");
     return;
 }
 
-static void _c_invoke_func(void (*pFunc)(void))
+
+static void _schedule(unsigned to)
 {
-    pFunc();
+    
     return;
 }
 
 
-static void _func_A()
+static void _coroutine_did_end()
 {
-    for (unsigned tmp = 0; tmp < 10; tmp ++)
+    AMCPrintf("A coroutine ends");
+
+    return;
+}
+
+
+static void _func_A(void)
+{
+    for (unsigned tmp = 0; tmp < 5; tmp++)
     {
-        AMCPrintf("A: tmp = %02u", tmp);
-        usleep(5000000);
+        AMCPrintf("A: %02u", tmp);
+        _schedule(1);
     }
     return;
 }
 
 
-static void _func_B()
+static void _func_B(void)
 {
-    for (unsigned tmp = 0; tmp < 10; tmp ++)
+    for (unsigned tmp = 0; tmp < 5; tmp++)
     {
-        AMCPrintf("B: tmp = %02u", tmp);
-        usleep(5000000);
+        AMCPrintf("B: %02u", tmp);
+        _schedule(0);
     }
     return;
 }
+
+static void _init_contexts(Context_st pContexts[2])
+{
+    int mmapFlags = MAP_PRIVATE | MAP_ANONYMOUS;
+    //int mmapFlags = MAP_SHARED;
+    const size_t STACK_SIZE = 128;
+
+    AMCPrintf("&(g_contexts[0]) = %p", &(pContexts[0]));
+    AMCPrintf("&(g_contexts[1]) = %p", &(pContexts[1]));
+
+    asm_init_context(&(pContexts[0]));
+    AMCPrintf("Before: g_contexts[0].rsp: %p", (void*)(pContexts[0].rsp));
+    pContexts[0].rip = (uint64_t)_coroutine_did_end;
+    pContexts[0].break_point = (uint64_t)_func_A;
+    pContexts[0].rsp = (uint64_t)mmap(NULL, STACK_SIZE, PROT_READ | PROT_WRITE, mmapFlags, -1, 0);
+    pContexts[0].rsp += STACK_SIZE - sizeof(uint64_t);
+
+    asm_init_context(&(pContexts[1]));
+    pContexts[1].rip = (uint64_t)_coroutine_did_end;
+    pContexts[1].break_point = (uint64_t)_func_B;
+    pContexts[1].rsp = (uint64_t)mmap(NULL, STACK_SIZE, PROT_READ | PROT_WRITE, mmapFlags, -1, 0);
+    pContexts[1].rsp += STACK_SIZE - sizeof(uint64_t);
+
+    //mprotect((void*)(g_contexts[0].rsp), STACK_SIZE, PROT_NONE);
+    //mprotect((void*)(g_contexts[1].rsp), STACK_SIZE, PROT_NONE);
+
+    uint64_t *pTest = (uint64_t*)(pContexts[0].rsp - STACK_SIZE);
+    pTest[0] = 0x123456789ABCDEF0;
+    pTest[(STACK_SIZE / sizeof(uint64_t)) - 1] = 0x123456789ABCDEF0;
+    AMCPrintf("After:  g_contexts[0].rsp: %p", (void*)(pContexts[0].rsp));
+    AMCPrintf("After:  g_contexts[1].rsp: %p", (void*)(pContexts[1].rsp));
+    AMCPrintf("main rsp: %p", asm_get_stack_addr());
+
+    AMCDataDump((const void *)pTest, STACK_SIZE);
+
+    return;
+}
+
+
+
 
 
 /**********/
@@ -85,9 +162,14 @@ static int _true_main(int argc, char* argv[])
 #error Upsupported CPU archetecture.
 #endif
 
-    AMCPrintf("sizeof(void*) = %u", (unsigned)sizeof(void*));
-    _c_invoke_func(_func_test_invoke);
-    asm_invoke_func((void*)_func_test_invoke);
+    Context_st contexts[2];
+    Context_st mainContexts;
+
+    AMCPrintf("sizeof(void *) = %u", (unsigned)sizeof(void *));
+    AMCPrintf("sizeof(Context_st *) = %u", (unsigned)sizeof(Context_st *));
+    _init_contexts(contexts);
+    _run_coroutines(&mainContexts, contexts);
+    AMCPrintf("coroutine ends");
 
 	/****/
 	/* ENDS */
